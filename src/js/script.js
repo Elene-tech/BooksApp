26dc442d89c0425f02e26a74aa2401fbc546dfbc
Olena Kurilets <elenakurilets@gmail.com>
2023-02-13 14:33:06 +0100

    Add OOP transform

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-// TASK #1
 ('use strict');
 const selected = dataSource.books;
 
@@ -27,97 +26,116 @@ const templates = {
   ),
 };
 
-function renderInList() {
-  for (const book of selected) {
-    const ratingBgc = determineRatingBgc(book.rating);
-    const ratingWidth = book.rating * 10;
+class BooksList {
+  constructor() {
+    const thisBooksList = this;
+    thisBooksList.favoriteBooks = [];
+    thisBooksList.filters = [];
 
-    book.ratingBgc = ratingBgc;
-    console.log('ratingBgc', ratingBgc);
-    book.ratingWidth = ratingWidth;
-    console.log('ratingWidth', ratingWidth);
-
-    /* generate HTML based on template (генерувати HTML на основі шаблона)*/
-    const generatedHTML = templates.books(book);
-
-    /* create element using utils.createElementFromHTML(створити  елемент за допомогою utils.createElementFromHTML)*/
-    const generatedDOMElement = utils.createDOMFromHTML(generatedHTML);
-
-    /* find menu container (знайти контейнер )*/
-    const bookContainer = document.querySelector(select.containerOf.container);
-
-    /* add element to menu (додати елемент до меню)*/
-    bookContainer.appendChild(generatedDOMElement);
+    //thisBooksList.getElements();
+    thisBooksList.initData();
+    thisBooksList.renderInList();
+    thisBooksList.initActions();
+    thisBooksList.determineRatingBgc();
   }
-}
-renderInList();
+  initData() {
+    const thisBooksList = this;
+    thisBooksList.data = dataSource.books;
+  }
+  determineRatingBgc(rating) {
+    const thisBooksList = this;
+    let background = '';
+    if (rating < 6) {
+      background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) {
+      background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if (rating > 8 && rating <= 9) {
+      background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if (rating > 9) {
+      background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+    return background;
+  }
+  renderInList() {
+    const thisBooksList = this;
 
-const favoriteBooks = [];
-console.log('favoriteBooks', favoriteBooks);
-/*додавання нової порожньої favoriteBooks*/
+    for (const book of thisBooksList.data) {
+      const ratingBgc = determineRatingBgc(book.rating);
+      const ratingWidth = book.rating * 10;
 
-function initActions() {
-  /*Додайте функцію initActions*/
-  const listBooksAndImages = document.querySelectorAll(
-    select.image.imageWrapper
-  ); //Підготуйте референціі на список усіх елементів .book__image у .booksList.
-  console.log('listBooksAndImages', listBooksAndImages);
+      book.ratingBgc = ratingBgc;
 
-  for (const listBookAndImage of listBooksAndImages) {
-    //Підготуйте референціі на список усіх елементів .book__image у .booksList.
-    listBookAndImage.addEventListener('dblclick', (e) => {
-      e.preventDefault();
-      listBookAndImage.classList.toggle('favorite'); //adds favourite heart when clicked
-      //Для кожного з них додайте прослуховувач, який, коли буде виявлений, запустить функцію, яка ...
-      // зупинить поведінку браузера за замовчуванням (preventDefault),
+      book.ratingWidth = ratingWidth;
 
-      /*Task 3 перевіря, чи книга вже є у «вибраних». Якщо це не так, то функція повинна працювати, як і раніше, 
-      тому додайте favorite і збережіть id книги favoriteBooks. Однак, якщо це так, сценарій повинен видалити 
-      id книги з масиву favoriteBooks та видалити клас favorite*/
+      const generatedHTML = templates.books(book);
 
-      if (!favoriteBooks.listBookAndImage) {
-        favoriteBooks.push(listBookAndImage);
-      }
-      if (favoriteBooks.listBookAndImage) {
-        favoriteBooks.remove(listBookAndImage);
-      }
-      //task 4
-      if (e.target.offsetParent.classList.contains('.book__image')) {
-        e.target.offsetParent.classList.add('favorite');
-        /* якщо натиснутий елемент містить його classList клас book__image (classList.contains ('book__image'), 
+      /* create element using utils.createElementFromHTML(створити  елемент за допомогою utils.createElementFromHTML)*/
+      const generatedDOMElement = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container (знайти контейнер )*/
+      const bookContainer = document.querySelector(
+        select.containerOf.container
+      );
+
+      /* add element to menu (додати елемент до меню)*/
+      bookContainer.appendChild(generatedDOMElement);
+    }
+  }
+  initActions() {
+    const thisBooksList = this;
+    /*Додайте функцію initActions*/
+    const listBooksAndImages = document.querySelectorAll(
+      select.image.imageWrapper
+    ); //Підготуйте референціі на список усіх елементів .book__image у .booksList.
+    console.log('listBooksAndImages', listBooksAndImages);
+
+    for (const listBookAndImage of listBooksAndImages) {
+      //Підготуйте референціі на список усіх елементів .book__image у .booksList.
+      listBookAndImage.addEventListener('dblclick', (e) => {
+        e.preventDefault();
+        listBookAndImage.classList.toggle('favorite');
+        if (!favoriteBooks.listBookAndImage) {
+          favoriteBooks.push(listBookAndImage);
+        }
+        if (favoriteBooks.listBookAndImage) {
+          favoriteBooks.remove(listBookAndImage);
+        }
+        //task 4
+        if (e.target.offsetParent.classList.contains('.book__image')) {
+          e.target.offsetParent.classList.add('favorite');
+          /* якщо натиснутий елемент містить його classList клас book__image (classList.contains ('book__image'), 
         то додаємо до натиснутого елементу 'favorite' */
-        //const bookId = document.getElementsByName(dataSource.books.id);
-        const bookId = listBookAndImage.dataset.id;
-        console.log('bookId', bookId);
-        favoriteBooks.push(bookId); /* додаємо id в масив */
+          //const bookId = document.getElementsByName(dataSource.books.id);
+          const bookId = listBookAndImage.dataset.id;
+          console.log('bookId', bookId);
+          favoriteBooks.push(bookId); /* додаємо id в масив */
+        }
+        if (e.target.offsetParent.classList.contains('.active')) {
+          /* перевіряємо, чи натиснутий елемент має клас active і якщо він є, то видаляємо з клікнутого елементу атрибут і клас favorite*/
+          //const bookId = document.getElementsByName(dataSource.books.id);
+          const bookId = listBookAndImage.dataset.id;
+          favoriteBooks.removeAttribute(bookId);
+          e.target.offsetParent.classList.remove('favorite');
+        }
+      });
+    }
+
+    const filtersForm = document.querySelector('.filters');
+    console.log('filtersForm', filtersForm);
+
+    filtersForm.addEventListener('click', function (event) {
+      if (event.target.checked) {
+        filters.push(event.target.value);
+        console.log(event.target.value);
+      } else {
+        filters.splice(event.target.value);
       }
-      if (e.target.offsetParent.classList.contains('.active')) {
-        /* перевіряємо, чи натиснутий елемент має клас active і якщо він є, то видаляємо з клікнутого елементу атрибут і клас favorite*/
-        //const bookId = document.getElementsByName(dataSource.books.id);
-        const bookId = listBookAndImage.dataset.id;
-        favoriteBooks.removeAttribute(bookId);
-        e.target.offsetParent.classList.remove('favorite');
-      }
+      filterBooks();
     });
   }
-  const filters = [];
-  console.log('filters', filters);
 
-  const filtersForm = document.querySelector('.filters');
-  console.log('filtersForm', filtersForm);
-
-  filtersForm.addEventListener('click', function (event) {
-    if (event.target.checked) {
-      filters.push(event.target.value);
-      console.log(event.target.value);
-    } else {
-      filters.splice(event.target.value);
-    }
-    filterBooks();
-  });
-
-  //Task 4
-  function filterBooks() {
+  filterBooks() {
+    const thisBooksList = this;
     const books = selected;
 
     for (let book of books) {
@@ -140,18 +158,5 @@ function initActions() {
     }
   }
 }
-initActions();
 
-function determineRatingBgc(rating) {
-  let background = '';
-  if (rating < 6) {
-    background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
-  } else if (rating > 6 && rating <= 8) {
-    background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
-  } else if (rating > 8 && rating <= 9) {
-    background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
-  } else if (rating > 9) {
-    background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
-  }
-  return background;
-}
+new BooksList();
